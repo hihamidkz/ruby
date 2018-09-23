d05a8d1bb0989f8f895e1886bdf9bbf256c27b16
hamid <hihamidkz@inbox.ru>
2018-09-23 14:21:20 +0700

    Created class for converting degrees

--- task1.rb
+++ task1.rb
@@ -6,4 +6,11 @@ scale1 = gets.chomp
 puts 'Input the second scale (C, K, F)'
 scale2 = gets.chomp
 
+class DegConverter
+    def initialize(deg, scale1, scale2)
+        @deg = deg
+        @scale1 = scale1
+        @scale2 = scale2
+    end
+end
 
